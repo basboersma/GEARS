bd17cb1f459874ca0079aeba0ea12e247346aff9
basboersma <basjjboersma@icloud.com>
2026-08-13 21:08:30 +0200

    -

--- lib/auth.ts
+++ lib/auth.ts
@@ -32,7 +32,7 @@ export const auth = betterAuth({
     google: {
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-      hd: "student.rug.nl", // Only allow Google accounts with @student.rug.nl domain
+      allowedDomains: ["student.rug.nl"], // <-- Use this
     },
   },
   emailAndPassword: {
