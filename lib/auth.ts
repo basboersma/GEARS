cd557c94bc00bf4960747d849d3126abde99a464
basboersma <basjjboersma@icloud.com>
2026-08-13 22:35:22 +0200

    Reverted Authorisation Restriction

--- lib/auth.ts
+++ lib/auth.ts
@@ -32,7 +32,7 @@ export const auth = betterAuth({
     google: {
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-      //allowedDomains: ["student.rug.nl"], // <-- Use this
+      allowedDomains: ["student.rug.nl"], // <-- Use this
     },
   },
   emailAndPassword: {
